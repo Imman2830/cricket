0fd1ab8656f4248526f6e136607dff68fac1b3ae
imman2830 <immanuvelab28@gmail.com>
2023-02-10 23:32:12 +0530

    India

--- src/test/java/org/git/GitPull.java
+++ src/test/java/org/git/GitPull.java
@@ -1,5 +1,16 @@
 package org.git;
 
 public class GitPull {
+	private void ind() {
+		System.out.println("india");
+
+	}
+	private void aus() {
+		System.out.println("Australia");
+
+	}
+	private void usa() {
+		System.out.println("Unated stats");
+	}
 
 }
