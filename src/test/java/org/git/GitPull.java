d0bfad453e2a2c42d8bf07f727ef6aaa5d8198e3
imman2830 <immanuvelab28@gmail.com>
2023-02-10 23:46:01 +0530

    Win

--- src/test/java/org/git/GitPull.java
+++ src/test/java/org/git/GitPull.java
@@ -1,5 +1,13 @@
 package org.git;
 
 public class GitPull {
-
+	private void Eng() {
+		System.out.println("England");
+	}
+	private void SAF() {
+		System.out.println("South Africa");
+	}
+private void PAk() {
+	System.out.println("Pakistan");
+}
 }
